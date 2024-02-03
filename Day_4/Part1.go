8d9fdaeb3c47897b1d61e2c9825485574e649400
Vincent <44277279+Arcey-bot@users.noreply.github.com>
2024-02-03 01:59:50 -0500

    input parsing complete

--- Day_4/Part1.go
+++ Day_4/Part1.go
@@ -5,12 +5,53 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"math"
+	"strconv"
 )
+
+type Card struct {
+	id int
+	winners, selection []int
+}
 
 const FILE string = "input.txt"
 func main() {
-	data := load_data(FILE)
-	fmt.Println(data)
+	data := to_cards(load_data(FILE))
+	for _, v := range data {
+		fmt.Println(v)
+	}
+}
+
+func card_value(card Card) (val int) {
+
+
+	// value is defined as: 2^0 = 1, 2^1 = 2, 2^2= 4, ...
+	return int(math.Pow(2, float64(val)))
+}
+
+func to_cards(in [][]string) []Card {
+	out := make([]Card, len(in))
+
+	for i, r := range in {
+		c := Card{
+			i+1,
+			to_ints(strings.Split(strings.Trim(r[0], " "), " ")),
+			to_ints(strings.Split(strings.Trim(r[1], " "), " ")),
+		}
+		out = append(out, c)
+	}
+
+	return out
+}
+
+func to_ints(in []string) []int {
+	out := make([]int, len(in))
+
+	for i, n := range in {
+		out[i], _ = strconv.Atoi(n)
+	}
+
+	return out
 }
 
 // index + 1 is card number
